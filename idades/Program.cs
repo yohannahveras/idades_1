9daa0f5b92d6e065099b12831d102a543cd5b7d8
Yohannah Véras <yohannahlv@gmail.com>
2023-06-10 16:45:20 -0300

    first commit

---
+++ idades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            string nome1, nome2;
+            int idade1, idade2;
+            double media;
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.Write("Nome: ");
+            nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.Write("Nome: ");
+            nome2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            idade2 = int.Parse(Console.ReadLine());
+
+            media = (idade1 + idade2) / 2.0;
+
+            Console.WriteLine("A idade média de " + nome1 + " e de " + nome2 + " é de " + media.ToString("F1", CI) + " anos.");
+
+
+        }
+    }
+}
